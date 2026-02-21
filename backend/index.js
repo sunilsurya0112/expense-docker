df19956c054741ee8885359f0b37ee2fc783f15a
sunilsurya0112 <sunilbujji373@gmail.com>
2026-02-21 15:39:51 +0530

    green

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 //Health Checking
 app.get('/health',(req,res)=>{
-    res.json("This is the health check for blue 1.0.0");
+    res.json("This is the health check for green 2.0.0");
 });
 
 // ADD TRANSACTION
